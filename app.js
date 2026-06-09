f80257445f56092d19ea9ea393baa71702d932cb
LeahJKH <leahjb06.kodehode@gmail.com>
2026-06-09 10:49:50 +0200

    end of class

--- app.js
+++ app.js
@@ -1,2 +1,34 @@
 const Card = document.querySelector("#pokeKort")
 // https://pokeapi.co/api/v2/pokemon/1/
+
+let pokemon;
+
+function ApiCall() {
+    console.log(pokemon)
+    const p = document.createElement("p")
+    const text = document.createTextNode(pokemon.name);
+    p.append(text)
+    Card.appendChild(p)
+}
+
+async function Poke(u, index) {
+    const res = await fetch(u + index);
+    const data = await res.json();
+    pokemon = await data;
+    ApiCall()
+}
+
+let pokeurl = "https://pokeapi.co/api/v2/pokemon/"
+
+for(let i = 0; i <= 1000; i++) {
+    Poke(pokeurl, i)
+    // pokeurl? console.log("jeg eksisterer"): console.log("jeg eksisterer ikke :(")
+}
+
+
+// let close = false
+// while(close === false) {
+//     console.log("hei på dei")
+// }
+
+// `hei hei ${}`
